e79f2c5612b5e95388eb7fbf3db608999ee83f23
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-02-07 16:00:12 +0300

    completed : 1-1 & 1-2

--- Series1/Program.cs
+++ Series1/Program.cs
@@ -2,6 +2,7 @@
 {
   private static void Main(string[] args)
   {
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine("1-1 was a course intro");
+    Console.WriteLine("1-2 talks about .net architecture, c# vs .net and the CLR and IR defs");
   }
 }
